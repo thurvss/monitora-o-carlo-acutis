6bbdcf23ec85c39159fb9c63aae02ae4e8f3432b
Arthur <thuralvarenga1104@gmail.com>
2026-02-16 17:35:53 -0400

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,11 +2,11 @@ import requests
 import time
 from datetime import datetime
 
-URL = "https://anjodajuventude.com.br/#pedido"  # coloque a URL exata da página
-BOT_TOKEN = "8305296309:AAG1iFblD59AhmxllWDdkj-0CkDynhgRPQ8"
+URL = "https://anjodajuventude.com.br/"
+BOT_TOKEN = "8305296309:AAEtnnYV9HIe6hv-KO8I_nNCz-l1Pm1lAS8"
 CHAT_ID = "7100064741"
 
-CHECK_INTERVAL = 30  # segundos
+CHECK_INTERVAL = 30
 PHRASE_BLOCKED = "envio de novos pedidos está suspenso"
 
 def send_telegram(message):
@@ -26,7 +26,13 @@ last_status = None
 
 while True:
     try:
-        response = requests.get(URL, timeout=10)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/120.0.0.0 Safari/537.36"
+        }
+
+        response = requests.get(URL, headers=headers, timeout=10)
         page = response.text.lower()
 
         current_status = "FECHADO" if is_blocked(page) else "ABERTO"
@@ -34,23 +40,22 @@ while True:
 
         if last_status is None:
             last_status = current_status
-            print(f"Status inicial: {current_status}")
+            print("Status inicial:", current_status)
 
         elif current_status != last_status:
 
             if current_status == "ABERTO":
-                for i in range(3):
+                for _ in range(3):
                     send_telegram(
                         f"🟢 PEDIDOS ABERTOS!\n"
-                        f"Horário detectado: {now}\n"
-                        f"Acesse: {URL}"
+                        f"Horário: {now}\n"
+                        f"{URL}"
                     )
                     time.sleep(2)
 
             elif current_status == "FECHADO":
                 send_telegram(
-                    f"🔴 PEDIDOS FECHARAM.\n"
-                    f"Horário detectado: {now}"
+                    f"🔴 PEDIDOS FECHARAM.\nHorário: {now}"
                 )
 
             last_status = current_status
